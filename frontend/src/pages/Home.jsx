53925dc6fe159f023cf0404e10416682fb15e4e6
Francislie <mfrancislie@gmail.com>
2023-08-06 11:38:21 -0700

    implement server, reactrouter and fetching products by useReducer

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+const Home = () => {
+  const initialState = {
+    products: [],
+    loading: true,
+    error: '',
+  };
+
+  const reducer = (action, state) => {
+    switch (action.type) {
+      case 'FETCH_REQUEST':
+        return { ...state, loading: true };
+      case 'FETCH_SUCCESS':
+        return { ...state, products: action.payload, loading: false };
+      case 'FETCH_FAIL':
+        return { ...state, error: action.payload, loading: false };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { loading, error, products } = state;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: error.message });
+      }
+      // setProducts(result.data);
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.slug}>
+              <Link to={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </Link>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
